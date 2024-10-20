6b5d07254654cc2c5417546542f46ff25f2fa9bc
AnojAryal <anoj1810@gmail.com>
2024-10-20 13:31:32 +0545

    creating empty migration

--- storefront/settings.py
+++ storefront/settings.py
@@ -81,10 +81,16 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# settings.py
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "storefront",
+        "USER": "franzy",
+        "PASSWORD": "fastrack0%",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
