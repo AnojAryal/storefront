ac19a12fd742e050452cde0d94ee01cef937ab60
AnojAryal <anoj1810@gmail.com>
2024-10-21 15:15:06 +0545

    adding inventory objects

--- store/admin.py
+++ store/admin.py
@@ -5,4 +5,22 @@ from . import models
 admin.site.register(models.Collection)
 
 
-admin.site.register(models.Product)
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "price", "inventory_status"]
+    list_editable = ["price"]
+    list_per_page = 10
+
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return "Low"
+        return "Ok"
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email", "membership"]
+    list_editable = ["membership"]
+    ordering = ["first_name", "last_name"]
+    list_per_page = 10
