ecfb373fed939707b29c099b015eb096bb6a2fda
AnojAryal <anoj1810@gmail.com>
2024-10-24 10:45:46 +0545

    adding searching adn filtering to the list page

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from django.db.models.aggregates import Count
+from django.db.models import QuerySet
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
 from . import models
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = "inventory"
+    parameter_name = "inventory"
+
+    def lookups(self, request, model_admin):
+        return [("<10", "Low")]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == "<10":
+            return queryset.filter(inventory_lt=10)
 
 
 @admin.register(models.Collection)
@@ -33,6 +46,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ["title", "price", "inventory_status", "collection_title"]
     list_editable = ["price"]
     list_per_page = 10
+    list_filter = ["collection", "last_update", InventoryFilter]
     list_select_related = ["collection"]
 
     def collection_title(self, product):
@@ -49,8 +63,9 @@ class ProductAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "email", "membership"]
     list_editable = ["membership"]
-    ordering = ["first_name", "last_name"]
     list_per_page = 10
+    ordering = ["first_name__istartswith", "last_name__istartswith"]
+    search_fields = ["first_name", "last_name"]
 
 
 @admin.register(models.Order)
