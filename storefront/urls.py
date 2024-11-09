93faa6257f4e6dcf61eefa8b173e5344a3b69051
AnojAryal <anoj1810@gmail.com>
2024-11-09 11:44:07 +0545

    creating views and urls

--- storefront/urls.py
+++ storefront/urls.py
@@ -25,4 +25,5 @@ admin.site.index_title = "Admin"
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),
+    path("store/", include("store.urls")),
 ]
