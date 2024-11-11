3f745e1c8f8344d8cfb7a8d27572c6ea3a560e08
AnojAryal <anoj1810@gmail.com>
2024-11-11 15:10:02 +0545

    serailizing relationships using pk, string, nested object and hyperlink

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import get_object_or_404
 import rest_framework
 from rest_framework.decorators import api_view
@@ -13,8 +14,12 @@ from .seralizers import ProductSerializer
 
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
-    seralizer = ProductSerializer(queryset, many=True)
+    queryset = Product.objects.select_related("collection").all()
+    seralizer = ProductSerializer(
+        queryset,
+        many=True,
+        context={"request": request},
+    )
     return Response(seralizer.data)
 
 
@@ -23,3 +28,8 @@ def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    return Response("Ok")
