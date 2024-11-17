e2034db1979296fb6de8105ab6afeff0fb703463
AnojAryal <anoj1810@gmail.com>
2024-11-17 11:08:19 +0545

    updating products

--- store/serializers.py
+++ store/serializers.py
@@ -10,24 +10,22 @@ class CollectionSerializer(serializers.Serializer):
         model = Collection
         fields = ["id", "title"]
 
-    # id = serializers.IntegerField()
-    # title = serializers.CharField(max_length=255)
-
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["id", "title", "price", "price_with_tax", "collection"]
-
-    # id = serializers.IntegerField()
-    # title = serializers.CharField(max_length=255)
-    # price = serializers.DecimalField(max_digits=6, decimal_places=2)
+        fields = [
+            "id",
+            "title",
+            "description",
+            "slug",
+            "inventory",
+            "price",
+            "price_with_tax",
+            "collection",
+        ]
 
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name="collection-detail",
-    )
 
     def calculate_tax(self, product: Product):
         return product.price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
-from .seralizers import ProductSerializer
+from .serializers import ProductSerializer
 
 
 # Create your views here.
@@ -25,15 +25,21 @@ def product_list(request):
     elif request.method == "POST":
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.validated_data
-        return Response("OK")
+        serializer.save()
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 
 @api_view()
