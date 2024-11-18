15ab1b3d73780b15f1fbdcc7b3373f4d51907863
AnojAryal <anoj1810@gmail.com>
2024-11-18 18:24:30 +0545

    creating generic views

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
 from django.http import HttpResponse
 import rest_framework
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -15,21 +16,12 @@ from .serializers import CollectionSerializer, ProductSerializer
 
 
 # class based view
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.select_related("collection").all()
-        seralizer = ProductSerializer(
-            queryset,
-            many=True,
-            context={"request": request},
-        )
-        return Response(seralizer.data)
+class ProductList(ListCreateAPIView):
+    queryset = Product.objects.select_related("collection").all()
+    serializer_class = ProductSerializer
 
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def get_serializer_context(self):
+        return {"request": self.request}
 
 
 class ProductDetail(APIView):
