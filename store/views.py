ed96ba1db8206042f98a451aec47e8d2b3db7d0c
AnojAryal <anoj1810@gmail.com>
2024-11-22 17:36:11 +0545

    pagination in django

--- store/views.py
+++ store/views.py
@@ -5,7 +5,8 @@ from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import status
 
-from store.filters import ProductFilter
+from .filters import ProductFilter
+from .pagination import DefaultPagination
 from .models import OrderItem, Product, Collection, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
@@ -19,6 +20,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ["title", "description"]
     ordering_fields = ["price", "last_update"]
 
