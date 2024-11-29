e175c2277415561a412e9e436adec7b7cc8bf5c1
AnojAryal <anoj1810@gmail.com>
2024-11-29 17:36:41 +0545

    get a cart

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,9 @@
 # converts a model instance to a python dictonary
 
+from dataclasses import fields
 from decimal import Decimal
 from rest_framework import serializers
-from store.models import Cart, Collection, Product, Review
+from store.models import Cart, CartItem, Collection, Product, Review
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -43,9 +44,32 @@ class ReviewSerializer(serializers.ModelSerializer):
         return Review.objects.create(product_id=product_id, **validated_data)
 
 
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "title", "price"]
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, cart_item: CartItem):
+        return cart_item.quantity * cart_item.product.price
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product", "quantity", "total_price"]
+
+
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True)  # Use CartItemSerializer for items
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, cart: Cart):
+        return sum([item.quantity * item.product.price for item in cart.items.all()])
 
     class Meta:
         model = Cart
-        fields = ["id"]
+        fields = ["id", "items", "total_price"]
