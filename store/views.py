82ef7933c5196cd1db8df6e03772c5614efb271f
AnojAryal <anoj1810@gmail.com>
2024-12-02 18:42:35 +0545

    posting a cartitem

--- store/views.py
+++ store/views.py
@@ -15,6 +15,7 @@ from .filters import ProductFilter
 from .pagination import DefaultPagination
 from .models import Cart, CartItem, OrderItem, Product, Collection, Review
 from .serializers import (
+    AddCartItemSerializer,
     CartItemSerializer,
     CartSerializer,
     CollectionSerializer,
@@ -88,10 +89,16 @@ class CartViewSet(
 
 
 class CartItemViewSet(ModelViewSet):
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return AddCartItemSerializer
+        return CartItemSerializer
 
-    serializer_class = CartItemSerializer
+    def get_serializer_context(self):
+        return {"cart_id": self.kwargs.get("cart_pk")}
 
     def get_queryset(self):
-        return CartItem.objects.filter(cart_id=self.kwargs["cart_pk"]).select_related(
-            "product"
-        )
+        cart_pk = self.kwargs.get("cart_pk")
+        if cart_pk is None:
+            raise ValueError("cart_pk is required in the URL to filter cart items.")
+        return CartItem.objects.filter(cart_id=cart_pk).select_related("product")
